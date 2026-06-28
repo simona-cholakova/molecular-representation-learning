5f408e0e2031e574b32d4a0c5b79b8a789015144
Simona Cholakova <colakovas@yahoo.com>
2026-06-28 10:17:41 +0200

    Encoder Layer done

--- scripts/molecular_representation.py
+++ scripts/molecular_representation.py
@@ -359,3 +359,30 @@ class FeedForward(nn.Module):
         x = self.dropout(x)
         x = self.loinear2(x) #[batch_size, seq_length, embed_dim]
         return x
+    
+class EncoderLayer(nn.Module):
+    def __init__(self, embed_dim, num_heads, ff_dim, dropout=0.1):
+        super().__init__()
+        #2 sublayers
+        self.attention = MultiHeadAttention(embed_dim, num_heads, dropout)
+        self.feed_forward = FeedForward(embed_dim, ff_dim, dropout)
+
+        #layer normalization after each sublayer
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.norm2 = nn.LayerNorm(embed_dim)
+
+        self.dropout = nn.Dropout()
+
+    def forward(self, x, attention_mask=None):
+        #x shape: [batch_size, seq_length, embed_dim]
+
+        #Sublayer 1: multihead attention + residual + norm
+        attended = self.attention(x, attention_mask)
+        x = self.norm1(x + self.dropout(attended))
+        #x + attended = residual connection
+        #norm1 = layer normalization
+
+        #Sublayer 2: feed-forward + residual + norm
+        f_forward = self.feed_forward(x)
+        x = self.norm2(x + self.dropout(f_forward))
+        return x
